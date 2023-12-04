f6a0133989435417d48f1d9ae5a90d54e2549277
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-12-04 16:57:36 +0530

    mongoose connect

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
- const express=require('express')
- const mongo=require('mongoose')
- 
+const express = require('express')
+const mongoose = require('mongoose')
+
+require('dotenv').config();
+
+const app = express();
+
+
+const port = process.env.SERVER_PORT;
+mongoose.connect('mongodb://localhost:27017/customer_mongo').then(() => {
+    app.listen(port, () => {
+        console.log(`api start & running  :${port}`)
+    })
+
+})clear
+
+
+
