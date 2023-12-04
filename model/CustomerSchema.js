0ed448452ea01e942bff94779394b1dafa42fe60
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-12-04 17:28:05 +0530

    customerSchema create

--- model/CustomerSchema.js
+++ model/CustomerSchema.js
@@ -1,1 +1,10 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
+
+const customerSchema = new mongoose.Schema({
+    nic:{type:String,require:true},
+    name:{type:String,require:true},
+    address:{type:String,require:true},
+    salary:{type:Number,require:true}
+});
+
+module.exports=mongoose.model('Customer',customerSchema)
