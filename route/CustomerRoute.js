3ae79e57e10be92d370903aea962a894e711fdca
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-12-04 18:27:49 +0530

    CustomerRoute finalize

---
+++ route/CustomerRoute.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const router = express.Router();
+
+const customerController = require('../controller/CustomerController');
+
+router.post('/save-customer',customerController.saveCustomer);
+router.put('update-customer',customerController.updateCustomer);
+router.delete('/delete-customer',customerController.deleteCustomer);
+router.get('/get-customer',customerController.findCustomer);
+router.get('/get-all-customer',customerController.findAllCustomer)
+
+exports.module=router
